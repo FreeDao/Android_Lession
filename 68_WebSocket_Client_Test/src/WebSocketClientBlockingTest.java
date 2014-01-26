2022f0f2a612d5766aff81eec59b3e316e7c6599
irelandKen <fengxue2026@qq.com>
2014-01-26 11:42:15 +0800

    Add Test For WebSocketClient connect Blocking

---
+++ 68_WebSocket_Client_Test/src/WebSocketClientBlockingTest.java
@@ -0,0 +1,69 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.handshake.ServerHandshake;
+
+/** 
+ * This example demonstrates how to create a websocket connection to a server. 
+ * 
+ * Only the most important callbacks are overloaded.
+ *  
+ */
+class ExampleClient2 extends WebSocketClient {
+
+	public ExampleClient2( URI serverUri , Draft draft ) {
+		super( serverUri, draft );
+	}
+
+	public ExampleClient2( URI serverURI ) {
+		super( serverURI );
+	}
+
+	@Override
+	public void onOpen( ServerHandshake handshakedata ) {
+		System.out.println( "opened connection" );
+		// if you pan to refuse connection based on ip or httpfields overload: onWebsocketHandshakeReceivedAsClient
+	}
+
+	@Override
+	public void onMessage( String message ) {
+		System.out.println( "received : " + message );
+		
+		String msg = "Hello " + Math.random();
+		
+		send( msg );
+		System.out.println( "send : " + msg );
+	}
+	
+	@Override
+	public void onMessage( ByteBuffer bytes ) {
+	};
+
+	@Override
+	public void onClose( int code, String reason, boolean remote ) {
+		// The codecodes are documented in class org.java_websocket.framing.CloseFrame
+		System.out.println( "Connection closed by " + ( remote ? "remote peer" : "us" ) );
+	}
+
+	@Override
+	public void onError( Exception ex ) {
+		ex.printStackTrace();
+		// if the error is fatal then onClose will be called additionally
+	}
+}
+
+public class WebSocketClientBlockingTest {
+
+	public static void main( String[] args ) throws URISyntaxException, InterruptedException {
+		ExampleClient2 c = new ExampleClient2( new URI( "ws://127.0.0.1/" ), new Draft_17() ); // more about drafts here: http://github.com/TooTallNate/Java-WebSocket/wiki/Drafts
+		boolean success = c.connectBlocking();
+		
+		c.send( "Come in" );
+		
+		Thread.currentThread().sleep(1000000000);
+	}
+}
